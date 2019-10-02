ab8dd518fb68a745b2a8f563b56b31ae2cbaf317
Farhan Yudhi Fatah <farhanscience@hotmail.com>
2019-10-02 15:53:47 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -9,3 +9,18 @@ SELECT * FROM users LEFT JOIN(
          UNION 
          SELECT * FROM pariwisata
       ) foo ON users.id_user = foo.id_admin
+
+
+// if something ambiguous
+
+SELECT id_user, U.nama AS penulis, foo.nama, tgl FROM users U LEFT JOIN(
+         SELECT * FROM budaya 
+         UNION 
+         SELECT * FROM bumd 
+         UNION 
+         SELECT * FROM kerajinan_masyarakat 
+         UNION 
+         SELECT * FROM kuliner 
+         UNION 
+         SELECT * FROM pariwisata
+      ) foo ON U.id_user = foo.id_admin
